607adade5178c813fc017f6afbfc17697abcd59c
AlinValentin7 <trandafiralinvalentin@gmail.com>
2024-09-26 18:08:52 +0200

    add: product2 and getInfo()

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,17 +1,37 @@
 package org.lessons.java.shop;
-
+import java.util.*;
 public class Main {
 	public static void main(String[] args) {
-		Prodotto oil = new Prodotto();
-		oil.name = "Olio di oliva quasi vergine ";
-		oil.priece= 9.99;
-		oil.iva = 22;
-		oil.code = 123;
-		oil.getPrezzoConIva();;
-		oil.codiceEstesto();
-		//System.out.println(oil.codiceEstesto());
-		oil.getCode();
-		//System.out.println(oil.getCode());
-		oil.getPrezzoBase();
+		Prodotto product = new Prodotto();
+		product.name = "Olio";
+		product.description = "Olio di oliva quasi vergine ";
+		product.price= 9.99;
+		product.iva = 22;
+		product.code = 123;
+		product.getPrezzoConIva();;
+		product.codiceEstesto();
+		//System.out.println(product.codiceEstesto());
+		product.getCode();
+		//System.out.println(product.getCode());
+		product.getPrezzoBase();
+		Scanner scan = new Scanner(System.in);
+		System.out.println("insereisci il nome del prodotto");
+		Prodotto product2= new Prodotto();
+		product2.name = scan.nextLine();
+		System.out.println("insereisci la descrizione del prodotto");
+		product2.description = scan.nextLine();
+		System.out.println("insereisci il prezzo senza iva del prodotto");
+		product2.price = scan.nextDouble();
+		System.out.println("insereisci l'iva del prodotto");
+		product2.iva = scan.nextDouble();
+		System.out.println("Il prezzo con iva e ugaula e a " + product2.getPrezzoConIva());
+		System.out.println("Il codice del prodotto e " + product2.getCode());
+		System.out.println("Le info del prodotto inserito sono "+ product2.getInfoProdotto());
+		
+		
+		
+		
+		
+		
 	}
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -5,19 +5,19 @@ public class Prodotto {
 	int code;
 	String name;
 	String description;
-	double priece;
+	double price;
 	double iva ;
 	
-	void getPrezzoConIva () {
-		double prezzoIntero = Math.floor(((priece * iva) / 100) + priece);
-		System.out.println(prezzoIntero + "$");
-		//return iva;
+	double getPrezzoConIva () {
+		double prezzoIntero = Math.round((((price * iva) / 100) + price)*100)/100;
+		//System.out.println(prezzoIntero + "$");
+		return prezzoIntero;
 	}
-	void getCode() {
+	int getCode() {
 		Random randomCode = new Random();
 		 code = randomCode.nextInt();
-		// return code;
-		 System.out.println("il codice generato è " + code);
+		 return code;
+		// System.out.println("il codice generato è " + code);
 	}
 	void codiceEstesto() {
 		/*Random randomCode = new Random();
@@ -27,9 +27,14 @@ public class Prodotto {
 		System.out.println(nome);
 	}
 	void getPrezzoBase() {
-		System.out.println(priece + "$");
+		System.out.println(price + "$");
 	}
-	
+	String getInfoProdotto() {
+		String Info = "nome del prodtto "+ name + " descrizione del prodotto "
+				+ description + " prezzo del prodotto " + price
+				+ " Iva del prodotto " + iva;
+		return Info;
+	}
 	
 }
 
